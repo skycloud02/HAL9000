4701f61b0c2bcba1e087324bd5044a360d03c550
Istvan Csaszar <icsaszar@outlook.com>
2020-10-10 18:41:46 +0300

    Update copy_um_apps_to_vm.cmd

--- postbuild/copy_um_apps_to_vm.cmd
+++ postbuild/copy_um_apps_to_vm.cmd
@@ -7,16 +7,32 @@ cd %~dp0%
 
 call paths.cmd
 
+if [%VOL_MOUNT_LETTER%]==[__EMPTY__] goto fail
 if [%PATH_TO_VM_DISK%]==[__EMPTY__] goto fail
+if [%PATH_TO_VM_TOOLS%]==[__EMPTY__] goto fail
 
+
+rem                                                               | Without this here there is an error: 
+rem                                                               |   "The filename, directory name, or volume label syntax is incorrect."
+rem                                                               | There should probably be a nicer way to do things...
+rem                                                               v 
+set PATH_TO_VM_MOUNT=%PATH_TO_VM_TOOLS:~0,-1%\bin\vmware-mount.exe"
 set PATH_TO_BINARIES=%1
-set FULL_PATH_TO_DESTINATION=[MountLetter]:\%2
+set FULL_PATH_TO_DESTINATION=%VOL_MOUNT_LETTER%\%2
 
-echo "    --------------------------- IMPORTANT ------------------------------"
-echo "    1. Please manually mount (with write permissions) the disk of HAL9000 found at: %PATH_TO_VM_DISK%"
-echo "    2. Copy the executables found at %PATH_TO_BINARIES% to %FULL_PATH_TO_DESTINATION%"
-echo "    3. Disconnect the mounted disk. Now you can run HAL9000 with updated user applications!"
-echo "    ------------------------------ END ---------------------------------"
+echo "Path to VMWARE mount is [%PATH_TO_VM_MOUNT%]"
+echo "Path to UM apps is [%PATH_TO_BINARIES%]"
+echo "Full path to destination is [%FULL_PATH_TO_DESTINATION%]"
+
+rem Mount the drive:
+echo %PATH_TO_VM_MOUNT% %VOL_MOUNT_LETTER% %PATH_TO_VM_DISK%
+%PATH_TO_VM_MOUNT% %VOL_MOUNT_LETTER% %PATH_TO_VM_DISK%
+
+rem Copy the applications
+xcopy /F /Y %PATH_TO_BINARIES%*.exe %FULL_PATH_TO_DESTINATION%
+
+rem Unmount the drive:
+%PATH_TO_VM_MOUNT% /d %VOL_MOUNT_LETTER%
 
 goto end
 
@@ -27,6 +43,6 @@ exit /b 1
 
 :end
 
-:: --- reload initial current directory, always exit with status != 0 so that build repeats this step ---
+:: --- reload initial current directory ---
 popd
-exit /b 1
+exit /b %ERRORLEVEL%
