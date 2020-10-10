3da4295bd318af927d861a3e1b606e796f77f87b
Istvan Csaszar <icsaszar@outlook.com>
2020-10-10 19:06:12 +0300

    Updated script to not fail when VMware disk tools are not installed

--- postbuild/setup_paths.pl
+++ postbuild/setup_paths.pl
@@ -64,20 +64,7 @@ if (!-d $vmwarePath) {
     }
 }
 
-my $vmtoolsPath = catfile($ENV{'PROGRAMFILES(x86)'}, "VMware", "VMware Virtual Disk Development Kit");
-if (!-d $vmtoolsPath) {
-    print sprintf("Could not find VMware Virtual Disk Development Kit in it's default location! %s\n", $vmtoolsPath);
-    if ($numArgs != 2) {
-        die "VMware Virtual Disk Development Kit not found, please specify install location of VMware Virtual Disk Development Kit as a command line argument to continue HAL setup\n";
-    }
-    $vmtoolsPath = $ARGV[1];
-    if (!-d $vmtoolsPath) {
-        die sprintf("Specified path: %s is not an existing directory\n", $vmtoolsPath);
-    }
-}
-
 print sprintf("VMware path is: %s\n", $vmwarePath);
-print sprintf("VMware Virtual Disk Development Kit path is: %s\n", $vmtoolsPath);
 
 # We need to parse the paths.cmd file and complete with this computer's paths
 print sprintf("Computer name is %s\n", $ENV{'COMPUTERNAME'});
@@ -90,7 +77,22 @@ my %hOptions =
     'PATH_TO_VIX_TOOLS' => $vmwarePath,
     'PATH_TO_LOG_FILE' => catfile($vmPaths, "HAL9000_VM", "HAL9000.log"),
     'PATH_TO_VM_FILE' => catfile($vmPaths, "HAL9000_VM", "HAL9000.vmx"),
-    'PATH_TO_VM_TOOLS' => $vmtoolsPath
 );
+
+my $vmtoolsPath = catfile($ENV{'PROGRAMFILES(x86)'}, "VMware", "VMware Virtual Disk Development Kit");
+if (!-d $vmtoolsPath) {
+    print sprintf("Could not find VMware Virtual Disk Development Kit in it's default location! %s\n", $vmtoolsPath);
+    if ($numArgs == 2) {
+        $vmtoolsPath = $ARGV[1];
+        if (!-d $vmtoolsPath) {
+            die sprintf("Specified path: %s is not an existing directory\n", $vmtoolsPath);
+        }
+        $hOptions{'PATH_TO_VM_TOOLS'} = $vmtoolsPath;
+        print sprintf("VMware Virtual Disk Development Kit path is: %s\n", $vmtoolsPath);
+    }
+} else {
+    $hOptions{'PATH_TO_VM_TOOLS'} = $vmtoolsPath;
+    print sprintf("VMware Virtual Disk Development Kit path is: %s\n", $vmtoolsPath);
+}
 
 SetupPathsCmdFile( catfile($projectRootBasePath, "postbuild", "paths.cmd"), $ENV{'COMPUTERNAME'}, \%hOptions);
