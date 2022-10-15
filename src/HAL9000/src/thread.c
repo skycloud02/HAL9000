0f59ce00f8cf8eb9d23668fe1dfaa83d18cc36dc
acolesa <adrian.colesa@cs.utcluj.ro>
2022-10-15 13:37:15 +0300

    Mici corecturi la varianta lui Radu. Pus design-ul de la threads in structura cu toate capitolele.

--- src/HAL9000/src/thread.c
+++ src/HAL9000/src/thread.c
@@ -877,7 +877,6 @@ _ThreadSetupInitialState(
     pIst = (PINTERRUPT_STACK) pStack;
 
     pIst->Rip = (QWORD) StartFunction;
-    if (KernelStack)
     {
         pIst->CS = GdtMuGetCS64Supervisor();
         pIst->Rsp = (QWORD)(pIst + 1);
